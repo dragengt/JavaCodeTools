1a51522986ed6a0544b6dd71cba1ee8af30bb5d6
曾昭亮80231356 <zengzhaoliang@cmbchina.com>
2020-04-10 21:10:45 +0800

    package info生成；增加从文件中加载代码template模板；TODO：将template中的{}内容替换为目标值，并生成代码

--- JavaToolsBiz/Util/JavaSpringBootFileCreator.cs
+++ JavaToolsBiz/Util/JavaSpringBootFileCreator.cs
@@ -1,6 +1,7 @@
 ﻿using AppCommon.Util;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -26,6 +27,9 @@ namespace JavaToolsBiz.Util
              
         };
 
+        /// <summary>
+        /// 文件路径名匹配：
+        /// </summary>
         static string[] C_RegexFilePathToMatch = new string[] {
             "",                                                                         //NULL
             @"^([\w]:.*)\\controller\\(.*)\\([\w]*)Controller\.java",    //Controller
@@ -33,11 +37,34 @@ namespace JavaToolsBiz.Util
             @"^([\w]:.*)\\mapper\\(.*)\\([\w]*)Mapper\.java"            //Mapper
         };
 
+        /// <summary>
+        /// 文件路径名替换目标：
+        /// </summary>
         static string[] C_RegexFilePathToReplace = new string[] {
             "",                                                                     //NULL
-            @"$1\\controller\\$2\\$3Controller\.java",    //生成Controller目标
-            @"$1\\service\\$2\\$3Service\.java",             //生成Service目标
-            @"$1\\mapper\\$2\\$3Mapper\.java"           //生成Mapper目标
+            @"$1\controller\$2\$3Controller.java",    //生成Controller目标
+            @"$1\service\$2\$3Service.java",             //生成Service目标
+            @"$1\mapper\$2\$3Mapper.java"           //生成Mapper目标
+        };
+
+        /// <summary>
+        /// java文件中的package信息匹配：
+        /// </summary>
+        static string[] C_RegexPackagePathToMatch = new string[] {
+            "",                                                                          //NULL
+            @"package (.*)\.controller\.(.*);",                       //Controller
+            @"package (.*)\.service\.(.*);",                           //Service
+            @"package (.*)\.mapper\.(.*);"                          //Mapper
+        };
+
+        /// <summary>
+        /// java文件中的package信息替换目标：
+        /// </summary>
+        static string[] C_PackagePathToFill= new string[] {
+            "",                                                                          //NULL
+            @"package {0}.controller.{1};",                       //Controller
+            @"package {0}.service.{1};",                           //Service
+            @"package {0}.mapper.{1};"                          //Mapper
         };
 
         /// <summary>
@@ -46,7 +73,7 @@ namespace JavaToolsBiz.Util
         /// <param name="filenameList"></param>
         /// <param name="errorMsg"></param>
         /// <returns></returns>
-        public static SBFileType AnalyzeFiles(string[] filenameList,out string errorMsg)
+        public static SBFileType AnalyzeFiles(List<string> filenameList,out string errorMsg)
         {
             errorMsg = string.Empty;
             if(CommonUtil.IsNullOrEmpty(filenameList))
@@ -127,16 +154,111 @@ namespace JavaToolsBiz.Util
         /// <summary>
         /// 创建文件名对应的Java文件
         /// </summary>
-        /// <param name="filenameList"></param>
-        /// <param name="sbFileType"></param>
+        /// <param name="srcFileName">原文件，用于生成对应的目标类型</param>
+        /// <param name="tarFileType">目标文件类型</param>
         /// <returns></returns>
-        public static bool CreateFileFor(string fileName, SBFileType sbFileType)
+        public static bool CreateFileFor(string srcFileName, SBFileType srcFileType, SBFileType tarFileType)
         {
+            //获得目标文件名，并确保目录可用：
+            string tarFileName = GetFileNameFor(srcFileName, srcFileType, tarFileType);
+            TryCreateDirectoryFor(tarFileName);
 
+            //文件内容填充：
+            StringBuilder sbFileContent = FillFileContentFor(srcFileName,srcFileType, tarFileName, tarFileType);
 
-
+            File.WriteAllText(tarFileName, sbFileContent.ToString(), Encoding.UTF8);
 
             return false;
+        }
+
+
+        /// <summary>
+        /// 从源文件中，推断出目标文件内容：
+        /// </summary>
+        /// <param name="srcFileName"></param>
+        /// <param name="tarFileType"></param>
+        /// <param name="tarFileName"></param>
+        /// <returns></returns>
+        private static StringBuilder FillFileContentFor(string srcFileName, SBFileType srcFileType, string tarFileName, SBFileType tarFileType)
+        {
+            StringBuilder sb = new StringBuilder();
+            string srcCode = File.ReadAllText(srcFileName);
+
+            //填充package信息：
+            string packageInfo = CreatePackageInfoFrom(srcCode,srcFileType, tarFileType);
+            sb.AppendLine(packageInfo);
+
+
+            //读入代码模板内容
+            string templateCodeFileName = GetCodeTemplateFileNameFor(tarFileType);
+            string templateCodes = File.ReadAllText(templateCodeFileName);
+            
+            return sb;
+        }
+
+        private static string GetCodeTemplateFileNameFor(SBFileType tarFileType)
+        {
+            string fileName = null;
+            switch (tarFileType)
+            {
+                case SBFileType.Controller:
+                    fileName = "ControllerTemplate.txt";
+                    break;
+
+                case SBFileType.Service:
+                    fileName = "ServiceTemplate.txt";
+                    break;
+
+                case SBFileType.Mapper:
+                    fileName = "MapperTemplate.txt";
+                    break;
+
+                case SBFileType.ResourceMapper:
+                    fileName = "MapperResourceTemplate.txt";
+                    break;
+
+                default:
+                    throw new Exception("非支持类型："+tarFileType);
+            }
+            return "JavaCodeTemplate/" + fileName;
+        }
+
+        /// <summary>
+        /// 从源代码中匹配出package信息
+        /// </summary>
+        /// <param name="srcCode"></param>
+        /// <returns></returns>
+        private static string CreatePackageInfoFrom(string srcCode, SBFileType srcFileType, SBFileType tarFileType)
+        {
+            int srcIndex = (int)srcFileType;
+            var match = Regex.Match(srcCode, C_RegexPackagePathToMatch[srcIndex]);
+            if(match.Success)
+            {
+                return string.Format(C_PackagePathToFill[(int)tarFileType], match.Groups[1], match.Groups[2]);
+            }
+            else
+            {
+                throw new Exception("没有在文件中找到对应" + srcFileType + "的package信息，可能文件内容有点不对？");
+            }
+        }
+
+        private static void TryCreateDirectoryFor(string tarFileName)
+        {
+            var dirName = Path.GetDirectoryName(tarFileName);
+            if (!Directory.Exists(dirName ))
+            {
+                Directory.CreateDirectory(dirName);
+            }
+        }
+
+        private static string GetFileNameFor(string srcFileName, SBFileType srcFileType,SBFileType tarFileType)
+        {
+            int tarRegexIndex = (int)tarFileType;
+            int srcRegexIndex = (int)srcFileType;
+            
+            string tarFileName = Regex.Replace(srcFileName, C_RegexFilePathToMatch[srcRegexIndex], C_RegexFilePathToReplace[tarRegexIndex]);
+            
+            return tarFileName;
         }
     }
 }
